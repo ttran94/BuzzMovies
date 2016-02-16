7c5491dd02f7f9761339d8c4c91a16c22b559cf6
Andie Rocca <rocca.andie@gmail.com>
2016-02-15 20:10:24 -0500

    fixed bug

--- app/src/main/java/com/example/taitran/buzzmovie/controller/EditPasswordActivity.java
+++ app/src/main/java/com/example/taitran/buzzmovie/controller/EditPasswordActivity.java
@@ -23,7 +23,7 @@ public class EditPasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_edit_password);
 
     }
 
