52cd8683552e3ac4ef9d3485aa9d06be2da46790
Andie Rocca <rocca.andie@gmail.com>
2016-02-27 00:07:21 -0500

    search activity and url request/json parse - it just werks

--- app/src/main/java/com/example/taitran/buzzmovie/controller/SearchActivity.java
+++ app/src/main/java/com/example/taitran/buzzmovie/controller/SearchActivity.java
@@ -1,29 +1,23 @@
 package com.example.taitran.buzzmovie.controller;
 
-import android.app.Fragment;
-import android.app.ListActivity;
-import android.app.SearchManager;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SearchView;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
+import org.json.JSONArray;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.net.URL;
 import java.util.Iterator;
 import android.widget.ListAdapter;
@@ -34,35 +28,23 @@ import org.apache.commons.io.IOUtils;
 /**
  * Created by andie on 2/24/2016.
  */
-public class SearchActivity extends ListActivity {
+
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText searchEditText;
+    private ListView lView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        //TODO DONT DO THIS
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         // Get the intent, verify the action and get the query
-        ListView lView = ((ListView) findViewById(R.id.listView));
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.d("Search Activity", "Search for " + query);
-            try {
-                JSONObject json = getjsonResults(query);
-                //get title, year, etc. to display in search
-                String[] results = parseJson(json);
-                ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, results);
-                lView.setVisibility(View.VISIBLE);
-                lView.setAdapter(adapter);
-                //lView.setOnItemSelectedListener(this);
-            } catch (Exception e) {
-                CharSequence text = e.getMessage();
-                Context context = getApplicationContext();
-                int duration = Toast.LENGTH_SHORT;
-                Toast message = Toast.makeText(context, text, duration);
-                message.show();
-            }
-        }
-
-        //((Button) findViewById(R.id.okbutton)).setVisibility(View.VISIBLE);
+        lView = ((ListView) findViewById(R.id.listView));
+        searchEditText = (EditText) findViewById(R.id.searchBar);
     }
 
     /**
@@ -73,15 +55,14 @@ public class SearchActivity extends ListActivity {
      * @throws JSONException
      */
     public String[] parseJson (JSONObject json) throws JSONException{
-        Iterator<String> keys = json.keys();
-        String info = "";
+        JSONArray jsonArray = json.getJSONArray("Search");
+        String info;
         String[] results = new String[10];
         int i = 0;
-        while (keys.hasNext() && i < 10) {
-            JSONObject record = new JSONObject(json.get(keys.next()).toString());
-            info += record.getString("Title");
-            info += " | " + record.getString("Year");
-            info += " | " + record.getString("Type");
+        while (i < 10) {
+            info = jsonArray.getJSONObject(i).getString("Title");
+            info += " | " + jsonArray.getJSONObject(i).getString("Year");
+            info += " | " + jsonArray.getJSONObject(i).getString("Type");
             results[i] = info;
             i++;
         }
@@ -96,21 +77,24 @@ public class SearchActivity extends ListActivity {
      * @throws JSONException if problem with json result
      */
     public JSONObject getjsonResults (String query) throws IOException, JSONException{
+        //TODO learn some regex
+        query = query.replaceAll("\\s+", "%20");
+        Log.d("Search Activity", "Modified query: " + query);
         Log.d("Search Activity", "Opening stream to omdb");
         String url = "http://www.omdbapi.com/?s=" + query;
+        //TODO don't do this on the main thread
         InputStream stream = new URL(url).openStream();
         StringWriter writer = new StringWriter();
-        IOUtils.copy(stream, writer);
+        IOUtils.copy(stream, writer); //Charset.forName("UTF-8"));
+        String jsonText = writer.toString();
         stream.close();
+        writer.close();
         Log.d("Search Activity", "stream successful");
-        String jsonText = IOUtils.toString(stream, Charset.forName("UTF-8"));
         return new JSONObject(jsonText);
     }
 
-    /*public void okButtonPressed(View v) {
-        ListView lView = ((ListView) findViewById(R.id.listView));
-        Intent intent = getIntent();
-        String query = intent.getStringExtra(SearchManager.QUERY);
+    public void goButtonPressed(View v) throws IOException, JSONException{
+        String query = searchEditText.getText().toString();
         Log.d("Search Activity", "Search for " + query);
         try {
             JSONObject json = getjsonResults(query);
@@ -121,11 +105,19 @@ public class SearchActivity extends ListActivity {
             lView.setAdapter(adapter);
             //lView.setOnItemSelectedListener(this);
         } catch (Exception e) {
-            CharSequence text = e.getMessage();
             Context context = getApplicationContext();
             int duration = Toast.LENGTH_SHORT;
-            Toast message = Toast.makeText(context, text, duration);
+            //Have you tried turning it off and on again?
+            Toast message = Toast.makeText(context, "No results", duration);
             message.show();
         }
-    }*/
+    }
+
+    public void clearButtonPressed(View v) {
+        Log.d("Search Activity", "cancel button pressed");
+        lView.setVisibility(View.GONE);
+        lView.setAdapter(null);
+        //Intent dash = new Intent(this, Dashboard.class);
+        //startActivity(dash);
+    }
 }
