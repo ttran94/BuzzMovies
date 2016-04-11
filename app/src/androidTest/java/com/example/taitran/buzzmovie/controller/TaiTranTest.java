9aa1857fb8158ad7ea413f0775ceeb6e73749e51
taitran94 <taitran994@yahoo.com>
2016-04-10 20:45:36 -0700

    TaiTran Test

--- app/src/androidTest/java/com/example/taitran/buzzmovie/controller/TaiTranTest.java
+++ app/src/androidTest/java/com/example/taitran/buzzmovie/controller/TaiTranTest.java
@@ -36,11 +36,66 @@ public class TaiTranTest {
 
     @Test
     public void checkShortUsername() {
-        onView(withId(R.id.regEmail)).perform(typeText("asd@mail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regEmail)).perform(typeText("taitran@mail.com"), closeSoftKeyboard());
         onView(withId(R.id.regUserName)).perform(typeText("tai"), closeSoftKeyboard());
         onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
         onView(withId(R.id.submitBtn)).perform(click());
         onView(withText("Username is too short.")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkLongUserName() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitran@mail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("taitran123456789123456"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Username is too long.")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkValidUserName() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitran@mail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("tai tran"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Invalid Username")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkValidEmail() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitranmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("taitran66"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Invalid email address")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkValidPassword() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitran@yahoo.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("taitran66"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Invalid password")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkSuccessfullyRegistered() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitran123@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("taitran32131"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Registration Success")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+        Espresso.pressBack();
+    }
+
+    @Test
+    public void checkUsernameAleadyExists() {
+        onView(withId(R.id.regEmail)).perform(typeText("taitran123@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.regUserName)).perform(typeText("taitran32131"), closeSoftKeyboard());
+        onView(withId(R.id.regPass)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withText("Username already exists")).inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 
 }
